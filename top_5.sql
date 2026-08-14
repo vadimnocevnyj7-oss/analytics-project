ccf5e1561b96d598581b6c194ba74098baafb85b
vadimnocevnyj7-oss <vadimnocevnyj7@gmail.com>
2026-08-14 12:24:23 +0300

    Update top_5.sql

--- top_5.sql
+++ top_5.sql
@@ -1,11 +1,18 @@
--- ТОП-5 менторов по количеству сессий за последний полный месяц в предоставленном датасете.
--- Последний полный месяц — август 2022 года, поскольку данные за сентябрь заканчиваются 15.09.2022.
+-- ТОП-5 менторов по количеству успешных сессий за последний полный месяц в датасете
+
+WITH last_full_month AS (
+    SELECT
+        DATE_TRUNC('month', MAX(session_date_time)) - INTERVAL '1 month' AS month_start
+    FROM sessions
+)
 SELECT
     mentor_id,
     COUNT(*) AS sessions_count
 FROM sessions
-WHERE session_date_time >= '2022-08-01'
-  AND session_date_time < '2022-09-01'
+CROSS JOIN last_full_month
+WHERE session_status = 'finished'
+  AND session_date_time >= month_start
+  AND session_date_time < month_start + INTERVAL '1 month'
 GROUP BY mentor_id
 ORDER BY sessions_count DESC
 LIMIT 5;
